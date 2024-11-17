aef5ab2f4c3cbf062dbd3b82d06ef83bfff46bbd
AriCamposM <ari1011@hotmail.com>
2024-11-17 03:52:00 -0600

    feature/footerSocialMedia

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
-import FacebookLogo from '../../Images/footer__fb.png';
 import GithubLogo from '../../Images/footer__github.png';
+import LinkedinLogo from '../../Images/linkedin.png';
 
 import './footer.css';
 
@@ -16,16 +16,24 @@ function Footer ({handleHomeClick}) {
             <div className="footer__container">
                 <p className="footer__home" onClick={handleHomeClick}>Inicio</p>
                 <p className="footer__practicum">Practicum</p>
-                <img className="footer__github"  src={GithubLogo}/>
-                <img className="footer__facebook" src={FacebookLogo}/>
+                <a href="https://github.com/AriCamposM/news_explorer-full" target="_blank" rel="noopener noreferrer">
+                    <img className="footer__github" alt="Github Logo" src={GithubLogo}/>
+                </a>
+                <a href="https://www.linkedin.com/in/ari-campos/" target="_blank" rel="noopener noreferrer">
+                    <img className="footer__facebook" alt="Linkedin Logo" src={LinkedinLogo}/>
+                </a>
             </div>
 
             <div className="footer__container-mobile">
                 <div className="footer__home-container-mobile">
                   <p className="footer__home-mobile" onClick={handleHomeClick}>Inicio</p>
                   <div className="footer__logos-container-mobile">
-                    <img className="footer__github-mobile"  src={GithubLogo}/>
-                    <img className="footer__facebook-mobile" src={FacebookLogo}/>
+                    <a href="https://github.com/AriCamposM/news_explorer-full" target="_blank" rel="noopener noreferrer">
+                        <img className="footer__github-mobile" alt="Github Logo" src={GithubLogo}/>
+                    </a>
+                    <a className="footer_link" href="https://www.linkedin.com/in/ari-campos/" target="_blank" rel="noopener noreferrer">
+                        <img className="footer__facebook-mobile" alt="Linkedin Logo" src={LinkedinLogo}/>
+                    </a>
                   </div>
                 </div>
                 <p className="footer__practicum-mobile">Practicum</p>
